403d78ab2a8978e8252831e537a988e7967e14c4
Aaron Stromberg <aaron.stromberg@gmail.com>
2024-05-30 23:46:00 -0500

    add change_mode command. also update sway config to include ebc refresh shortcut, no ctrl, and default to tabbed layout

--- usr/local/bin/WinkShell/change_mode.sh
+++ usr/local/bin/WinkShell/change_mode.sh
@@ -2,18 +2,19 @@
 
 # Read input from the pipe
 input=$(cat)
+# input=$(echo "bw+dither\ngreyscale" | fuzzel -d)
 
 # Check the input and perform different actions based on it
 if [ "$input" = "bw+dither" ]; then
-    dbus-send --system --dest=org.pinenote.ebc --print-reply /ebc  org.pinenote.ebc.SetBwMode byte:1  
-    dbus-send --system --dest=org.pinenote.ebc --print-reply /ebc  org.pinenote.ebc.SetDefaultWaveform byte:1
-    # This might happen automatically? 
-    dbus-send --system --print-reply --dest=org.pinenote.ebc /ebc org.pinenote.ebc.TriggerGlobalRefresh
+	dbus-send --system --dest=org.pinenote.ebc --print-reply /ebc org.pinenote.ebc.SetBwMode byte:1
+	dbus-send --system --dest=org.pinenote.ebc --print-reply /ebc org.pinenote.ebc.SetDefaultWaveform byte:1
+	# This might happen automatically?
+	dbus-send --system --print-reply --dest=org.pinenote.ebc /ebc org.pinenote.ebc.TriggerGlobalRefresh
 elif [ "$input" = "greyscale" ]; then
-    dbus-send --system --dest=org.pinenote.ebc --print-reply /ebc  org.pinenote.ebc.SetBwMode byte:0  
-    dbus-send --system --dest=org.pinenote.ebc --print-reply /ebc  org.pinenote.ebc.SetDefaultWaveform byte:4
-    # This might happen automatically? 
-    dbus-send --system --print-reply --dest=org.pinenote.ebc /ebc org.pinenote.ebc.TriggerGlobalRefresh
+	dbus-send --system --dest=org.pinenote.ebc --print-reply /ebc org.pinenote.ebc.SetBwMode byte:0
+	dbus-send --system --dest=org.pinenote.ebc --print-reply /ebc org.pinenote.ebc.SetDefaultWaveform byte:4
+	# This might happen automatically?
+	dbus-send --system --print-reply --dest=org.pinenote.ebc /ebc org.pinenote.ebc.TriggerGlobalRefresh
 else
-    echo "Input not recognized"
+	echo "Input not recognized"
 fi
